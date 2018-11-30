e75456dbdc6bc6bfefceebe12e09931590c93854
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2018-11-30 13:10:52 +0100

    Website

---
+++ precache-manifest.cf486a685729dd988f6c09313779cd8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56fb1f153c5a922ae62b",
+    "url": "/static/js/main.56fb1f15.chunk.js"
+  },
+  {
+    "revision": "b79acb106b617191fdd2",
+    "url": "/static/js/1.b79acb10.chunk.js"
+  },
+  {
+    "revision": "56fb1f153c5a922ae62b",
+    "url": "/static/css/main.ff776306.chunk.css"
+  },
+  {
+    "revision": "b6dc514bf31bb0820de08e4bede828be",
+    "url": "/index.html"
+  }
+];
